a04fc0d8b9068a25ddb7359e1de492b5d91528b2
Gurpreet Singh <gurpreetsk@hotmail.com>
2020-04-11 19:23:31 +0530

    Final app demonstrating use of widgets and properties composibility.

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Center(
-          child: Text('Hello, World!'),
-        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: Text("I'm Rich"),
+              centerTitle: true,
+              backgroundColor: Colors.blueGrey[900],
+            ),
+            backgroundColor: Colors.blueGrey,
+            body: Center(
+              child: Image(
+                image: AssetImage("images/diamond.png"),
+              ),
+            )),
       ),
     );
